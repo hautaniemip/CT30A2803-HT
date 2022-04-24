aa3669b65985bf85845e0ce63bb0b713e54032a8
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-24 03:03:13 +0300

    Style changes

--- lib/styles.dart
+++ lib/styles.dart
@@ -4,30 +4,29 @@ class Styles {
 
   static ThemeData themeData(bool isDarkTheme, BuildContext context) {
     return ThemeData(
-      primarySwatch: Colors.red,
+      primarySwatch: Colors.green,
       primaryColor: isDarkTheme ? Colors.black : Colors.white,
 
-      backgroundColor: isDarkTheme ? Colors.black : Color(0xffF1F5FB),
+      backgroundColor: isDarkTheme ? Colors.black : const Color(0xffF1F5FB),
 
-      indicatorColor: isDarkTheme ? Color(0xff0E1D36) : Color(0xffCBDCF8),
-      buttonColor: isDarkTheme ? Color(0xff3B3B3B) : Color(0xffF1F5FB),
+      indicatorColor: isDarkTheme ? const Color(0xff0E1D36) : const Color(0xffCBDCF8),
 
-      hintColor: isDarkTheme ? Color(0xff280C0B) : Color(0xffEECED3),
+      hintColor: isDarkTheme ? const Color(0xff280C0B) : const Color(0xffEECED3),
 
-      highlightColor: isDarkTheme ? Color(0xff372901) : Color(0xffFCE192),
-      hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
+      highlightColor: isDarkTheme ? const Color(0xff372901) : const Color(0xffFCE192),
+      hoverColor: isDarkTheme ? const Color(0xb23a3a3b) : const Color(0xb34285f4),
 
-      focusColor: isDarkTheme ? Color(0xff0B2512) : Color(0xffA8DAB5),
+      focusColor: isDarkTheme ? const Color(0xff0B2512) : const Color(0xffA8DAB5),
       disabledColor: Colors.grey,
-      textSelectionColor: isDarkTheme ? Colors.white : Colors.black,
-      cardColor: isDarkTheme ? Color(0xFF151515) : Colors.white,
+      cardColor: isDarkTheme ? const Color(0xFF151515) : Colors.white,
       canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
       brightness: isDarkTheme ? Brightness.dark : Brightness.light,
       buttonTheme: Theme.of(context).buttonTheme.copyWith(
-          colorScheme: isDarkTheme ? ColorScheme.dark() : ColorScheme.light()),
-      appBarTheme: AppBarTheme(
-        elevation: 0.0,
-      ),
+          colorScheme: isDarkTheme ? const ColorScheme.dark() : const ColorScheme.light()),
+      appBarTheme: const AppBarTheme(
+          elevation: 0.0,
+        ),
+        textSelectionTheme: TextSelectionThemeData(selectionColor: isDarkTheme ? Colors.white : Colors.black),
     );
   }
 }
